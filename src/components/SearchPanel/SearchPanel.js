c286ca665be37c59589fb24e0e2216a593463461
Drekavac <diesel39931@gmail.com>
2019-04-12 13:30:59 +0600

    added style for components

---
+++ src/components/SearchPanel/SearchPanel.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './SearchPanel.css';
+
+const SearchPanel = () => {
+    return (<input type="text" placeholder="search" className="form-control search-input"/>);
+};
+
+export default SearchPanel
--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppHeader from './components/AppHeader';
-import SearchPanel from './components/SearchPanel';
-import TodoList from './components/TodoList';
+import AppHeader from './components/AppHeader/AppHeader';
+import SearchPanel from './components/SearchPanel/SearchPanel';
+import TodoList from './components/TodoList/TodoList';
+import ItemStatusFilter from './components/ItemStatusFilter/ItemStatusFilter';
+import './index.css';
 
 const App = () => {
     const todoData = [
-        { label: 'Drink Coffee', important: false },
-        { label: 'Make Awasome App', important: true },
-        { label: 'Have a lunch', important: false }
+        { label: 'Drink Coffee', important: false, id: 1 },
+        { label: 'Make Awesome App', important: true, id: 2 },
+        { label: 'Have a lunch', important: false, id: 3 }
     ];
 
     return (
-        <div>
-            <AppHeader />
-            <SearchPanel />
+        <div className="todo-app">
+            <AppHeader toDo={ 1 } done={ 3 }/>
+            <div className="todo-panel d-flex">
+                <SearchPanel />
+                <ItemStatusFilter />
+            </div>
             <TodoList todos={ todoData }/>
         </div>
     );
